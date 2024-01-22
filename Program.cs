10cacb9cf936baedc48db441585f86a8238fca09
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 18:22:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_10_Methods_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling methods, AKA functions
+
+            SayHi();
+
+            //Method with parameters
+
+            SayHiUser("Nompumelelo");
+
+            SayHiAge("Nompumelelo", 25);
+
+            
+            Console.ReadLine();
+
+
+        }
+        static void SayHi()
+        {
+            Console.WriteLine(" Hi ");
+        }
+        static void SayHiUser(string name)
+        { Console.WriteLine(" Hello " + name); 
+        }
+        static void SayHiAge(string name, int age)
+        { Console.WriteLine(" Hello " + name + " you are " + age);
+        }
+    }
+}
